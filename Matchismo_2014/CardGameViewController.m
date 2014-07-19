795d2dce4fd61e40832b3b342050aad01e995b51
wuqifu <wuqifu@live.cn>
2014-07-19 23:26:02 +0800

    Teacher’s solution.

--- Matchismo_2014/CardGameViewController.m
+++ Matchismo_2014/CardGameViewController.m
@@ -7,25 +7,25 @@
 //
 
 #import "CardGameViewController.h"
-#import "model/Deck.h"
-#import "model/PlayingCardDeck.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (nonatomic, strong) Deck *deck;
-
+@property (strong, nonatomic) Deck *deck;
 @end
 
 @implementation CardGameViewController
 
 -(Deck *)deck
 {
-    if (!_deck) {
-        _deck = [[PlayingCardDeck alloc] init];
-    }
-    
+    if (!_deck) _deck = [self createDeck];
     return _deck;
+}
+
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
 }
 
 -(void)setFlipCount:(int)flipCount
@@ -33,31 +33,24 @@
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
     NSLog(@"flipcount change to: %d", self.flipCount);
-    
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
-    
     if ([sender.currentTitle length]) {
         [sender setBackgroundImage:[UIImage imageNamed:@"cardback"]
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
+        self.flipCount++;
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        
         Card *card = [self.deck drawRandomCard];
         if (card != nil) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
             [sender setTitle:card.contents
                     forState:UIControlStateNormal];
-        } else {
-            NSLog(@"All cards has been fliped");
+            self.flipCount++;
         }
     }
-    
-    self.flipCount++;
 }
-
-
 
 @end
